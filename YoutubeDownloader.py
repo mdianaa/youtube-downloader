bccc887f921485d704ad0976a5ce59063baf8b66
Diana Mihaylova <d.mihaylova.2002@gmail.com>
2020-09-26 16:31:14 +0300

    The console app supports audio and video downloads for video downloads there is an option to download the video with the highest quality.

---
+++ YoutubeDownloader.py
@@ -0,0 +1,28 @@
+from pytube import YouTube
+
+
+class YoutubeDownloader:
+
+    @staticmethod
+    def download(urls, audio_only, highest_resolution, download_location):
+        for url in urls:
+            try:
+                youtube = YouTube(url)
+                video_title = youtube.title
+                print("Downloading:\nSong Title: {}".format(video_title))
+                if audio_only:
+                    stream = youtube.streams.filter(only_audio=True).first()
+                else:
+                    if highest_resolution:
+                        stream = youtube.streams.get_highest_resolution()
+                    else:
+                        stream = youtube.streams.first()
+                if download_location == '':
+                    stream.download()
+                else:
+                    stream.download(download_location)
+                print("{} successfully downloaded.".format(video_title))
+
+            except Exception as e:
+                print(e)
+                print("Something went wrong")
--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,19 +1,36 @@
-from pytube import YouTube
+from YoutubeDownloader import YoutubeDownloader
+from os import path
 
-url = str(input("Please specify video URL:"))
+url = str(input('Please specify youtube video URL:'))
+download_location = str(input("Specify download directory(full path). Press Enter to download in current directory:"))
+if download_location != '' and not path.exists(download_location):
+    print('Please enter a valid path!')
+    exit(1)
+highest_resolution = False
+audio_only = False
 
-try:
-    youtube = YouTube(url)
-    print("Downloading the following song:\n")
-    print("Song Title: {}\nSong Description: {}".format(youtube.title,
-                                                        youtube.description))
-    stream = youtube.streams.first()
-    stream.download()
-    print("Songs successfully downloaded.")
+while True:
+    audio_only_user_answer = str(input("Do you want to download only the audio of the video?")).lower()
+    if audio_only_user_answer == 'yes':
+        audio_only = True
+        break
+    elif audio_only_user_answer == 'no':
+        audio_only = False
+        while True:
+            highest_resolution_user_answer = str(
+                input("Do you want to download the video with the highest resolution?")).lower()
+            if highest_resolution_user_answer == 'yes':
+                highest_resolution = True
+                break
+            elif highest_resolution_user_answer == 'no':
+                highest_resolution = False
+                break
+            else:
+                print("Please enter either Yes or No. You entered {}".format(highest_resolution_user_answer))
+        break
+    else:
+        print("Please enter either Yes or No. You entered {}".format(audio_only_user_answer))
 
-except Exception as e:
-    print(e)
-    print("Something went wrong")
+urls = [url]
 
-
-# # python3.6 youtube_downloader.py
+YoutubeDownloader.download(urls, audio_only, highest_resolution, download_location)
